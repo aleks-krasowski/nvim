80a24c700f42477ec9ce0e25192b4174ed32c04c
Aleksander Krasowski <aleks.krasowski@outlook.de>
2026-09-03 10:48:03 +0200

    Add comment for remote-clipboard support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Support for copying to clipboard from remote
 if vim.env.SSH_TTY then
   local osc52 = require("vim.ui.clipboard.osc52")
   vim.g.clipboard = {
